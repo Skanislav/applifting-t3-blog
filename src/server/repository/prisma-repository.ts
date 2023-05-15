936306908b97d1e2c0dff797d93ec5a29cd8d25b
Skas 🥷 <skas.merkushin@gmail.com>
2023-05-15 13:15:28 +0200

    feat: prisma create article implementation

--- src/server/repository/prisma-repository.ts
+++ src/server/repository/prisma-repository.ts
@@ -4,6 +4,12 @@ import { prisma } from "~/server/db";
 export interface ArticlesRepository {
   getArticles: () => Promise<Article[]>;
   getBySlug: (slug: string) => Promise<Article>;
+  createArticle: (
+    title: string,
+    content: string,
+    image: string,
+    user_id: string
+  ) => Promise<Article>;
 }
 
 class PrismaArticlesRepository implements ArticlesRepository {
@@ -35,6 +41,30 @@ class PrismaArticlesRepository implements ArticlesRepository {
     }
 
     return Promise.reject(new Error("Article not found"));
+  }
+
+  async createArticle(
+    title: string,
+    content: string,
+    image_url: string,
+    author_name: string
+  ): Promise<Article> {
+    const created = await prisma.article.create({
+      data: {
+        title,
+        content,
+        image_url,
+        author_name,
+        slug: title.toLowerCase().replace(/ /g, "-"),
+        perex: content.substring(0, 100),
+      },
+    });
+
+    if (created) {
+      return Promise.resolve(created);
+    }
+
+    return Promise.reject(new Error("Article not created"));
   }
 }
 
