f704692b23444c31934071d3e65a29fe364720fe
Skas 🥷 <skas.merkushin@gmail.com>
2023-05-16 09:09:14 +0200

    chore: set up babel for .mjs module transpiling

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,6 +2,11 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  transform: {
+    "^.+\\.jsx?$": "babel-jest",
+    "^.+\\.m?js$": "babel-jest",
+  },
+  transformIgnorePatterns: ["/node_modules/"],
   moduleNameMapper: {
     "~/(.*)": "<rootDir>/src/$1",
   },
